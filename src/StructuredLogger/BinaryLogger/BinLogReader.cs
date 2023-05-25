631b77f9a6eca036a90b357460c1a4c4b82a9013
Rolf Bjarne Kvinge <rolf@xamarin.com>
2023-05-25 09:43:48 +0200

    Add timestamp logging.

--- src/StructuredLogger/BinaryLogger/BinLogReader.cs
+++ src/StructuredLogger/BinaryLogger/BinLogReader.cs
@@ -93,6 +93,7 @@ namespace Microsoft.Build.Logging.StructuredLogger
 
             var streamLength = stream.Length;
 
+            var allWatch = Stopwatch.StartNew();
             while (true)
             {
                 BuildEventArgs instance = null;
@@ -134,6 +135,9 @@ namespace Microsoft.Build.Logging.StructuredLogger
                     OnStringDictionaryComplete?.Invoke(strings);
                 }
             }
+
+            var elapsedAll = allWatch.Elapsed;
+            Console.WriteLine($"Read {recordsRead} records in {elapsedAll} = {elapsedAll.TotalMilliseconds} ms");
 
             if (progress != null)
             {
