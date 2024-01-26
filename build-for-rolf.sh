7245efb9e1f9cfb4357ca1268219f697b5a91587
Rolf Bjarne Kvinge <rolf@xamarin.com>
2024-01-26 07:38:27 +0100

    More rolf

--- build-for-rolf.sh
+++ build-for-rolf.sh
@@ -3,6 +3,8 @@
 set -o pipefail
 IFS=$'\n\t'
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 cd src/StructuredLogViewer.Avalonia
 if [[ $(arch) == "arm64" ]]; then
     RID=osx-arm64
@@ -10,3 +12,4 @@ else
     RID=osx-x64
 fi
 dotnet publish --self-contained -r $RID
+
